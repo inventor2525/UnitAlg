700d410404e5f07128edcfc1336d446b92a93069
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2020-12-29 19:01:18 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='UnitAlg',
+    version='0.0.1',
+    author='Charlie Angela Mehlenbeck',
+    author_email='charlie_inventor2003@yahoo.com',
+    packages=[],
+    scripts=[],
+    url='https://github.com/inventor2525/UnitAlg',
+    license='LICENSE.txt',
+    description='An un-described package.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        
+    ]
+)
