fcf10bb5203f145c2ac82e0576fb89f08c655abe
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2021-01-26 01:35:23 -0500

    fixed Transform Coefficents functions

--- UnitAlg/Transform.py
+++ UnitAlg/Transform.py
@@ -46,18 +46,18 @@ class Transform():
     def coefficients_2d(self):
         m = self._mat
         return [
-            mat[0,0],mat[0,1],
-            mat[1,0],mat[1,1],
-            mat[0,3],mat[1,3]]
+            m[0,0],m[0,1],
+            m[1,0],m[1,1],
+            m[0,3],m[1,3]]
 
     @property
     def coefficients_3d(self):
         m = self._mat
         return [
-            mat[0,0],mat[0,1],mat[0,2], 
-            mat[1,0],mat[1,1],mat[1,2], 
-            mat[2,0],mat[2,1],mat[2,2], 
-            mat[0,3],mat[1,3],mat[2,3]]
+            m[0,0],m[0,1],m[0,2], 
+            m[1,0],m[1,1],m[1,2], 
+            m[2,0],m[2,1],m[2,2], 
+            m[0,3],m[1,3],m[2,3]]
 
     @property
     def translation(self) -> np.ndarray:
