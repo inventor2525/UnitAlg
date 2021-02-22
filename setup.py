a645661cbc0e6c332bae542e120ae29affea5ae5
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2021-02-21 23:10:10 -0500

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='UnitAlg',
-    version='0.0.7',
+    version='0.0.8',
     author='Charlie Angela Mehlenbeck',
     author_email='charlie_inventor2003@yahoo.com',
     packages=['UnitAlg'],
