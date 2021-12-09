2448c48dcb854cc5dddde8f7e5dc939696a51a69
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2021-12-08 21:58:25 -0500

    Sorted Vector3 class

--- UnitAlg/Vector3.py
+++ UnitAlg/Vector3.py
@@ -9,54 +9,38 @@ class Vector3():
     def __init__(self, x:float, y:float, z:float=0):
         self.value = [x,y,z]
 
-    @staticmethod
-    def _from_np(value: np.ndarray) -> 'Vector3':
-        newArr = Vector3.__new__(Vector3)
-        newArr._value = value
-        return newArr
-
     #----Common values----
-    @classproperty
-    def zero() -> 'Vector3':
-        return Vector3(0,0,0)
-    
-    @classproperty
-    def left() -> 'Vector3':
-        return Vector3(-1,0,0)
-    
-    @classproperty
-    def right() -> 'Vector3':
-        return Vector3(1,0,0)
-
-    @classproperty
-    def down() -> 'Vector3':
-        return Vector3(0,-1,0)
-
-    @classproperty
-    def up() -> 'Vector3':
-        return Vector3(0,1,0)
-
     @classproperty
     def back() -> 'Vector3':
         return Vector3(0,0,-1)
-
+    @classproperty
+    def down() -> 'Vector3':
+        return Vector3(0,-1,0)
     @classproperty
     def forward() -> 'Vector3':
         return Vector3(0,0,1)
-
     @classproperty
-    def ones() -> 'Vector3':
+    def left() -> 'Vector3':
+        return Vector3(-1,0,0)
+    @classproperty
+    def negative_infinity() -> 'Vector3':
+        return Vector3(-math.inf,-math.inf,-math.inf)
+    @classproperty
+    def one() -> 'Vector3':
         return Vector3(1,1,1)
-
-    #----Casting----
-    #TODO: better python interface to use for this? aka "__Something__"
-    @staticmethod
-    def from_other(value) -> 'Vector3':
-        if isinstance(value, (gp_Vec, gp_Pnt, gp_Dir, gp_XYZ)):
-            return Vector3(value.X(), value.Y(), value.Z())
-        if isinstance(value, np.ndarray):
-            return Vector3(*value)
-
+    @classproperty
+    def positive_infinity() -> 'Vector3':
+        return Vector3(math.inf,math.inf,math.inf)
+    @classproperty
+    def right() -> 'Vector3':
+        return Vector3(1,0,0)
+    @classproperty
+    def up() -> 'Vector3':
+        return Vector3(0,1,0)
+    @classproperty
+    def zero() -> 'Vector3':
+        return Vector3(0,0,0)
+        
     #----Main Properties----
     @property
     def value(self) -> np.ndarray:
@@ -85,7 +69,22 @@ class Vector3():
     @z.setter
     def z(self, z:float) -> None:
         self._value[2] = z
-
+    
+    #----Casting----
+    @staticmethod
+    def _from_np(value: np.ndarray) -> 'Vector3':
+        newArr = Vector3.__new__(Vector3)
+        newArr._value = value
+        return newArr
+        
+    #TODO: better python interface to use for this? aka "__Something__"
+    @staticmethod
+    def from_other(value) -> 'Vector3':
+        if isinstance(value, (gp_Vec, gp_Pnt, gp_Dir, gp_XYZ)):
+            return Vector3(value.X(), value.Y(), value.Z())
+        if isinstance(value, np.ndarray):
+            return Vector3(*value)
+            
     #----Functions----
     def sq_magnitude(self) -> float:
         ''' Returns squared length of this vector '''
@@ -114,23 +113,23 @@ class Vector3():
     def distance(vector_a:'Vector3', vector_b:'Vector3') -> float:
         ''' Returns the distance between two vectors (same as (a-b).magnitude) '''
         return (vector_a - vector_b).magnitude()
-
-    @staticmethod
-    def dot(vector_a:'Vector3', vector_b:'Vector3') -> float:
-        ''' Dot product between two vectors '''
-        return np.dot(vector_a.value,vector_b.value)
-
-    @staticmethod
-    def cross(vector_a:'Vector3',vector_b:'Vector3') -> 'Vector3':
-        '''Cross product between two vectors '''
-        return Vector3._from_np(np.cross(vector_a.value,vector_b.value))
-
+    
+    @staticmethod 
     def angle(from_v:'Vector3', to_v:'Vector3') -> float:
         ''' 
         Returns the unsigned angle between 'fromV' and 'toV' in degrees.  
         Angle is never greater than 180.  
         '''
         return math.degrees(math.acos(Vector3.dot(from_v,to_v)/(from_v.magnitude()*to_v.magnitude())))
+    
+    @staticmethod
+    def cross(vector_a:'Vector3',vector_b:'Vector3') -> 'Vector3':
+        '''Cross product between two vectors '''
+        return Vector3._from_np(np.cross(vector_a.value,vector_b.value))
+    @staticmethod
+    def dot(vector_a:'Vector3', vector_b:'Vector3') -> float:
+        ''' Dot product between two vectors '''
+        return np.dot(vector_a.value,vector_b.value)
 
     @staticmethod
     def lerp(vector_a:'Vector3', vector_b:'Vector3', factor:float) -> 'Vector3':
@@ -142,41 +141,41 @@ class Vector3():
         return Vector3._from_np(p1 + v_norm*d*factor)
 
     #----Operators----
-    def __str__(self) -> str:
-        return str.format('({0}, {1}, {2})',*self._value)
-    def __repr__(self) -> str:
-        return self.__str__()
-    
-    def __iter__(self) -> Iterator[float]:
-        return self._value.__iter__()
-    def __len__(self) -> int:
-        return 3
-    
-    def __getitem__(self, index:int)->float:
-        return self._value[index]
-    def __setitem__(self,index:int,value:float) -> None:
-        self._value[index] = value
-        
-    def __eq__(self,other:'Vector3') -> bool:
-        return all(self.value == other.value)
-    def __ne__(self,other:'Vector3') -> bool:
-        return any(self.value != other.value)
-
     def __add__(self,other:'Vector3') -> 'Vector3':
         return Vector3._from_np(self.value + other.value)
     def __iadd__(self,other:'Vector3') -> 'Vector3':
         return self + other
     def __sub__(self,other:'Vector3') -> 'Vector3':
         return Vector3._from_np(self.value - other.value)
-
+    
     def __mul__(self,other:float) -> 'Vector3':
         return Vector3._from_np(self.value * other)
     def __truediv__(self,other:float) -> 'Vector3':
         return Vector3._from_np(np.divide(self.value, other))
-
+        
+    def __eq__(self,other:'Vector3') -> bool:
+        return all(self.value == other.value)
+    def __ne__(self,other:'Vector3') -> bool:
+        return any(self.value != other.value)
+        
+    def __getitem__(self, index:int)->float:
+        return self._value[index]
+    def __setitem__(self,index:int,value:float) -> None:
+        self._value[index] = value
+        
+    def __iter__(self) -> Iterator[float]:
+        return self._value.__iter__()
+    def __len__(self) -> int:
+        return 3
+        
     def __hash__(self) -> int:
         return hash((self.x,self.y,self.z))
-
+        
+    def __str__(self) -> str:
+        return str.format('({0}, {1}, {2})',*self._value)
+    def __repr__(self) -> str:
+        return self.__str__()
+    
     #----OCC conversion functions----
     def occ_Ax1(self, origin=[0,0,0]) -> gp_Ax1:
         return gp_Ax1(Vector3(origin).occ_Pnt, self.occ_Dir)
