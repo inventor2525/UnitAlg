543760d5012aa470763e531766b95c9ecdc2ca1e
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2021-12-09 18:32:53 -0500

    imports

--- UnitAlg/__init__.py
+++ UnitAlg/__init__.py
@@ -1,4 +1,5 @@
 from .Vector3 import Vector3
-# from .Quaternion import Quaternion
+from .Quaternion import Quaternion
+from .Ray import Ray
 # from .Transform import Transform
 # from .Plane import Plane
