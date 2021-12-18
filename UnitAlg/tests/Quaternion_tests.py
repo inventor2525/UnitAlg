73e368e3939ab208e7ee7f13cab7fe0f1990dc6b
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2021-12-18 18:35:24 -0500

    + Quaternion constructor test

--- UnitAlg/tests/Quaternion_tests.py
+++ UnitAlg/tests/Quaternion_tests.py
@@ -1,6 +1,11 @@
 import math
 import unittest
 from UnitAlg import *
+from math import isclose
+import numpy as np
+
+def sq_mag(v:np.ndarray) -> float:
+	return v[0]*v[0] + v[1]*v[1] + v[2]*v[2] + v[3]*v[3]
 
 class QuaternionTests(unittest.TestCase):	
 	def test00_constructors(self):
@@ -8,7 +13,21 @@ class QuaternionTests(unittest.TestCase):
 		Tests that the constructors produce a Quaternion 
 		correctly, and as a copy of any passed data.
 		'''
-		self.assertTrue(False)
+		q = Quaternion(1,3.14,42,2)
+		self.assertTrue(isclose(q.x,1) and isclose(q.y,3.14) and isclose(q.z,42) and isclose(q.w,2))
+		
+		_in = [-1,3,-5,-2]
+		q = Quaternion(_in)
+		_in[0] = 2
+		_in[1] += _in[2]
+		self.assertTrue(isclose(q.x,-1) and isclose(q.y,3) and isclose(q.z,-5) and isclose(q.w,-2))
+		
+		_in = np.array([99,98,97,-2])
+		q = Quaternion(_in)
+		_in[0] = 2
+		_in[1] += _in[2]
+		self.assertTrue(isclose(q.x,99) and isclose(q.y,98) and isclose(q.z,97) and isclose(q.w,0-2))
+		
 	def test01_properties(self):
 		'''Makes sure the Quaternion properties work.'''
 		self.assertTrue(False)
