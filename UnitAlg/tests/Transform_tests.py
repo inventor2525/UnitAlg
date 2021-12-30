acee87ef46e02fc77b77b1d670a0df81191c24e4
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2021-12-30 02:16:00 -0500

    + Transform multiply test

--- UnitAlg/tests/Transform_tests.py
+++ UnitAlg/tests/Transform_tests.py
@@ -2,14 +2,14 @@ import math
 import unittest
 from UnitAlg import *
 
-def quaternion_to_transform(q:Quaternion, scale:Vector3=Vector3(1,1,1)) -> Transform:
+def quaternion_to_transform(q:Quaternion, scale:Vector3=Vector3(1,1,1), translation:Vector3=Vector3(0,0,0)) -> Transform:
 	x = (q * Vector3(1,0,0))*scale.x
 	y = (q * Vector3(0,1,0))*scale.y
 	z = (q * Vector3(0,0,1))*scale.z
 	return Transform([
-		[x.x, y.x, z.x, 0],
-		[x.y, y.y, z.y, 0],
-		[x.z, y.z, z.z, 0],
+		[x.x, y.x, z.x, translation.x],
+		[x.y, y.y, z.y, translation.y],
+		[x.z, y.z, z.z, translation.z],
 		[0,   0,   0,   1]
 	])
 			
@@ -86,7 +86,72 @@ class TransformTests(unittest.TestCase):
 		'''
 		Tests multiplying Transforms (and Vectors).
 		'''
-		self.assertTrue(False)
+		def mul_TR_transform_vector3(t:Transform, v:Vector3) -> Vector3:
+			x = t._mat[0,0]*v.x + t._mat[0,1]*v.y + t._mat[0,2]*v.z #+ t._mat[0,3]*v.w
+			y = t._mat[1,0]*v.x + t._mat[1,1]*v.y + t._mat[1,2]*v.z #+ t._mat[1,3]*v.w
+			z = t._mat[2,0]*v.x + t._mat[2,1]*v.y + t._mat[2,2]*v.z #+ t._mat[2,3]*v.w
+			#w= t._mat[3,0]*v.x + t._mat[3,1]*v.y + t._mat[3,2]*v.z #+ t._mat[3,3]*v.w
+			return Vector3(x,y,z)
+		def test_rotation_only(q:Quaternion, v:Vector3) -> None:
+			t = Transform.Rotate(q)
+			self.assertTrue(t == quaternion_to_transform(q))
+			self.assertTrue(mul_TR_transform_vector3(t,v) == q*v)
+			self.assertTrue(mul_TR_transform_vector3(t,v) == t*v)
+			self.assertTrue(q*v == t*v)
+		rotations = [
+			Quaternion.identity,
+			Quaternion(1,0,0,0),
+			Quaternion(0,1,0,0),
+			Quaternion(0,0,1,0),
+			Quaternion(2,4,1,.5).normalized,
+			Quaternion(-4,1,8,-.35).normalized,
+			Quaternion(-4,1,8,-.35).normalized
+		]
+		for q in rotations:
+			test_rotation_only(q, Vector3(3,6,2))
+		
+		def test_translation_scale_seperate(modifier:Vector3, v:Vector3) -> None:
+			t = Transform.Translate(modifier)
+			self.assertTrue(t*v == v+modifier)
+			
+			t = Transform.Scale(modifier)
+			self.assertTrue(t*v == modifier*v)
+		
+		test_translation_scale_seperate(Vector3(0,0,0), Vector3(3,6,2))
+		test_translation_scale_seperate(Vector3(0,1,0), Vector3(3,6,2))
+		test_translation_scale_seperate(Vector3(2,0,7.3), Vector3(3,6,2))
+		test_translation_scale_seperate(Vector3(1,-6.2,9.6), Vector3(3,6,2))
+		test_translation_scale_seperate(Vector3(-1,-7,-3), Vector3(3,6,2))
+		
+		def test_TRS(translation:Vector3, rotation:Quaternion, scale:Vector3, v:Vector3) -> None:
+			t_test = quaternion_to_transform(rotation, scale, translation)
+			t = Transform.TRS(translation, rotation, scale)
+			self.assertTrue(t_test == t)
+			
+			self.assertTrue(t*v == rotation*(v*scale)+translation)
+			
+			#test transform*transform:
+			_t = Transform.Translate(translation)
+			_r = Transform.Rotate(rotation)
+			_s = Transform.Scale(scale)
+			self.assertTrue(t == _t*_r*_s )
+			
+		scales = [
+			Vector3(1,1,1),
+			Vector3(.3,.6,.2),
+			Vector3(3,1,-7),
+			Vector3(-3,1,-7),
+			Vector3(1,1,-1),
+			Vector3(1,-1,-1)
+		]
+		for s in scales:
+			for q in rotations:
+				test_TRS(Vector3(0,0,0), q, s, Vector3(3,6,2))
+				test_TRS(Vector3(0,1,0), q, s, Vector3(3,6,2))
+				test_TRS(Vector3(2,0,7.3), q, s, Vector3(3,6,2))
+				test_TRS(Vector3(1,-6.2,9.6), q, s, Vector3(3,6,2))
+				test_TRS(Vector3(-1,-7,-3), q, s, Vector3(3,6,2))
+		
 	def test04_inverse(self):
 		'''
 		Tests Transform.inverse
