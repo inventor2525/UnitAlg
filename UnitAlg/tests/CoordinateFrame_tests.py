499bab4f32d7bd9ad505c446b760f5e7129a8c94
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2022-01-13 17:18:49 -0500

    + frame conversion test

--- UnitAlg/tests/CoordinateFrame_tests.py
+++ UnitAlg/tests/CoordinateFrame_tests.py
@@ -28,6 +28,43 @@ class Vector3Tests(unittest.TestCase):
 		self.assertTrue(Vector3.left == Vector3(0,1,0))
 		self.assertTrue(Vector3.back == Vector3(-1,0,0))
 		self.assertTrue(Vector3.down == Vector3(0,0,-1))
+		
+	def test01_frame_conversion(self):
+		t = Transform.conversion_from_to(CoordinateFrame.Normal_Math, CoordinateFrame.Unity)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(1,0,0))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(0,0,1))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(0,1,0))
+		t = Transform.conversion_from_to(CoordinateFrame.Unity, CoordinateFrame.Normal_Math)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(1,0,0))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(0,0,1))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(0,1,0))
+		
+		t = Transform.conversion_from_to(CoordinateFrame.Normal_Math, CoordinateFrame.ROS)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(0,-1,0))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(1,0,0))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(0,0,1))
+		t = Transform.conversion_from_to(CoordinateFrame.ROS, CoordinateFrame.Normal_Math)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(0,1,0))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(-1,0,0))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(0,0,1))
+		
+		t = Transform.conversion_from_to(CoordinateFrame.Unity, CoordinateFrame.ROS)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(0,-1,0))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(0,0,1))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(1,0,0))
+		t = Transform.conversion_from_to(CoordinateFrame.ROS, CoordinateFrame.Unity)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(0,0,1))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(-1,0,0))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(0,1,0))
+		
+		t = Transform.conversion_from_to(CoordinateFrame.OpenCV, CoordinateFrame.ROS)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(0,-1,0))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(0,0,-1))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(1,0,0))
+		t = Transform.conversion_from_to(CoordinateFrame.ROS, CoordinateFrame.OpenCV)
+		self.assertTrue(t*Vector3(1,0,0) == Vector3(0,0,1))
+		self.assertTrue(t*Vector3(0,1,0) == Vector3(-1,0,0))
+		self.assertTrue(t*Vector3(0,0,1) == Vector3(0,-1,0))
 
 if __name__ == 'main':
 	unittest.main()
