a481017d0ca94db6b790b444ee5dbe514412f652
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2022-02-16 12:50:47 -0500

    fixed type hinting of class property

--- UnitAlg/helpers/classproperty.py
+++ UnitAlg/helpers/classproperty.py
@@ -1,12 +1,13 @@
-from typing import Iterator
+from typing import Callable, Iterator, TypeVar, Generic
 
-class classproperty():
-	def __init__(self, f):
+_T = TypeVar("_T", covariant=True)
+class classproperty(Generic[_T]):
+	def __init__(self, f:Callable[[],_T]):
 		self.f = f
-	def __get__(self, obj, owner):
+	def __get__(self, obj, owner) -> _T:
 		return self.f()
 
-def all_true(bools:Iterator[bool]):
+def all_true(bools:Iterator[bool]) -> bool:
 	'''
 	Checks if all in iterator are true.
 	'''
